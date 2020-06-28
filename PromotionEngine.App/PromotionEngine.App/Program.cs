3765801fffcb05de3c77c5ffb072de693398b294
Prashant Verma <prashantverma1112@gmail.com>
2020-06-28 22:26:45 +0530

    added name

--- PromotionEngine.App/PromotionEngine.App/Program.cs
+++ PromotionEngine.App/PromotionEngine.App/Program.cs
@@ -6,7 +6,7 @@ namespace PromotionEngine.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test given for promotion engine by Prashant Verma");
         }
     }
 }
